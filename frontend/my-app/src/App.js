748f1c275a7e40299944aad0f07d5ae40fe6c870
Varun Bharathi Jayakumar <jayakumar.va@varuns-air.lan>
2023-04-01 16:20:16 -0400

    added more features

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -7,20 +7,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddCourseForms from "./components/AddCourseForms";
 import NavBar from "./components/NavBar";
 import HomeCard from "./components/HomeCard";
-
+import { useState } from "react";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 function App() {
   return (
     <ChakraProvider>
-      <div>
-        <NavBar />
-      </div>
-      <Router>
-        <Routes>
-          <Route exact path="/learning" element={<HomeCard />}></Route>
-          <Route exact path="/" element={<Dashboard />}></Route>
-          <Route exact path="/add" element={<AddCourseForms />}></Route>
-        </Routes>
-      </Router>
+      <>
+        <Router>
+          <Routes>
+            <Route exact path="/login" element={<Login />}></Route>
+            <Route exact path="/signup" element={<Signup />}></Route>
+            <Route exact path="/learning" element={<HomeCard />}></Route>
+            <Route exact path="/" element={<Dashboard />}></Route>
+            <Route exact path="/add" element={<AddCourseForms />}></Route>
+          </Routes>
+        </Router>
+      </>
     </ChakraProvider>
   );
 }
---
+++ frontend/my-app/src/components/Signup.js
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import {
+  InputLeftAddon,
+  Flex,
+  Heading,
+  Input,
+  Button,
+  InputGroup,
+  Stack,
+  InputLeftElement,
+  chakra,
+  Box,
+  Link,
+  Avatar,
+  FormControl,
+  FormHelperText,
+  InputRightElement,
+} from "@chakra-ui/react";
+import { FaUserAlt, FaLock } from "react-icons/fa";
+import { BiWalletAlt } from "react-icons/bi";
+
+const CFaUserAlt = chakra(FaUserAlt);
+const CFaLock = chakra(FaLock);
+
+const Signup = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowClick = () => setShowPassword(!showPassword);
+
+  return (
+    <Flex
+      flexDirection="column"
+      width="100wh"
+      height="100vh"
+      backgroundColor="gray.200"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        flexDir="column"
+        mt="2"
+        mb="2"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Avatar bg="blue.600" />
+        <Heading color="blue.600">Welcome, Please Sign up</Heading>
+        <Box minW={{ base: "90%", md: "468px" }}>
+          <form>
+            <Stack
+              spacing={8}
+              p="1rem"
+              backgroundColor="whiteAlpha.900"
+              boxShadow="md"
+            >
+              <FormControl>
+                <Stack spacing={8}>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      children={<CFaUserAlt color="gray.300" />}
+                    />
+                    <Input type="text" placeholder="First name" />
+                  </InputGroup>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      children={<CFaUserAlt color="gray.300" />}
+                    />
+                    <Input type="text" placeholder="Last Name" />
+                  </InputGroup>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      children={<CFaUserAlt color="gray.300" />}
+                    />
+                    <Input type="text" placeholder="Wallet address" />
+                  </InputGroup>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      children={<BiWalletAlt color="gray.300" />}
+                    />
+                    <Input type="email" placeholder="email address" />
+                  </InputGroup>
+                  <InputGroup>
+                    <InputLeftAddon children="+234" />
+                    <Input type="tel" placeholder="phone number" />
+                  </InputGroup>
+                </Stack>
+              </FormControl>
+
+              <Button
+                borderRadius={0}
+                type="submit"
+                variant="solid"
+                colorScheme="Blue"
+                width="full"
+              >
+                Login
+              </Button>
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
+      <Button colorScheme="green">Signup</Button>
+    </Flex>
+  );
+};
+
+export default Signup;
