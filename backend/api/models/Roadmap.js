3522dcea66fd4efe624015fac7939572dfad7099
Varun Bharathi Jayakumar <jayakumar.va@varuns-air.lan>
2023-04-01 21:24:14 -0400

    roadmap unchanged

--- backend/api/models/Roadmap.js
+++ backend/api/models/Roadmap.js
@@ -1,29 +1,33 @@
 import mongoose from "mongoose";
 
-const RoadmapSchema = new mongoose.Schema(
+const RoadmapSchema = new mongoose.Schema({
+  Day: {
+    type: Number,
+  },
+  topics: [
     {
-        Day :{
-            type : Number
-          },
-        topics : [{
-            type : String
-        }],
-        assignments : [{
-            type : String
-        }],
-        mcqs : [{
-            type: mongoose.SchemaTypes.ObjectId,
-          }],
-        is_completed:{
-            type: Boolean,
-            default: false
-        },
-        is_locked :{
-            type: Boolean,
-            default: true
-        }
+      type: String,
+    },
+  ],
+  assignments: [
+    {
+      type: String,
+    },
+  ],
+  mcqs: [
+    {
+      type: mongoose.SchemaTypes.ObjectId,
+    },
+  ],
+  is_completed: {
+    type: Boolean,
+    default: false,
+  },
+  is_locked: {
+    type: Boolean,
+    default: true,
+  },
+});
 
-    });
-
-    export const Roadmap = mongoose.model("Roadmap", RoadmapSchema);
-    export default {Roadmap}
+export const Roadmap = mongoose.model("Roadmap", RoadmapSchema);
+export default { Roadmap };
