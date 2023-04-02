1156d7c79723edccddfe61425490f13dd23ef442
Varun Bharathi Jayakumar <jayakumar.va@varuns-air.lan>
2023-04-01 21:31:31 -0400

    changed model for education

--- backend/api/models/education.js
+++ backend/api/models/education.js
@@ -1,28 +1,35 @@
 import mongoose from "mongoose";
 
-const educationSchema = new mongoose.Schema(
+const educationSchema = new mongoose.Schema({
+  day: {
+    type: String,
+  },
+  hours: {
+    type: String,
+  },
+  level: {
+    type: String,
+  },
+  topic: {
+    type: String,
+  },
+  roadmap: [
     {
-        day: {
-            type : String
-        },
-        hours: {
-            type : String
-        },
-        level: {
-            type : String
-        },
-        topic: {
-            type : String
-        },
-        roadmap: [{
-            type: mongoose.SchemaTypes.ObjectId,
-            required: true
-          }],
-        user : [{
-            type: mongoose.SchemaTypes.ObjectId,
-        }]
-    });
+      type: mongoose.SchemaTypes.ObjectId,
+      required: true,
+    },
+  ],
+  user: [
+    {
+      type: mongoose.SchemaTypes.ObjectId,
+    },
+  ],
+  completed: {
+    type: Number,
+    default: 0,
+  },
+});
 
-    export const education = mongoose.model("education", educationSchema);
+export const education = mongoose.model("education", educationSchema);
 
-    export default {education}
+export default { education };
