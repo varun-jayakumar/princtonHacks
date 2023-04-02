db8b3c941d504884b8c2fa2e67b94ec7a7ddc27a
Varun Bharathi Jayakumar <jayakumar.va@varuns-air.lan>
2023-04-01 21:22:15 -0400

    added endpoint /dashboard

--- backend/api/controllers/dashboardController.js
+++ backend/api/controllers/dashboardController.js
@@ -1,4 +1,6 @@
 import { user } from "../models/user.js";
+import { education } from "./../models/education.js";
+
 const setResponse = (obj, response) => {
   response.status(200);
   response.json(obj);
@@ -13,14 +15,15 @@ const setError = (err, response) => {
 export const getLearnings = async (req, res) => {
   try {
     let id = JSON.parse(req.body);
-
-    // let userdata = await user.findOne(id);
+    let result = [];
+    let userdata = await user.findOne(id);
     let learningsArray = userdata.learnings;
-    learningsArray.array.forEach((learning) => {
-      console.log(learning);
+    learningsArray.array.forEach(async (learningID) => {
+      let edu = await education.findOne({ learning: learningID });
+      result.push(edu);
     });
 
-    setResponse({ Something: "Something" }, res);
+    setResponse({ learnings: result }, res);
   } catch (error) {
     setError(error, res);
   }
